2fe8f5df4034f644bd4a92159a98f0b91f187f7d
Gever Tulley <gever@sfbrightworks.org>
2015-08-31 23:08:41 -0700

    querying status records
    and trying to figure out templates

--- bowerbird.js
+++ bowerbird.js
@@ -4,19 +4,9 @@ PilotStatus = new Mongo.Collection('pilot_status');
  * CLIENT SIDE
  */
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
+  Template.messages.helpers({
+    'records' : function() {
+      return PilotStatus.find();
     }
   });
 }
@@ -30,7 +20,6 @@ if (Meteor.isServer) {
     // code to run on server at startup
     PilotStatus.allow({
       insert: function (userId, post) {
-        // TODO: check to make sure it's coming from Twilio...
         return true;
       }
       // since there is no definition for update or delete,
@@ -43,6 +32,7 @@ if (Meteor.isServer) {
         this.response.end('get request\n');
       })
       .post(function () {
+        // TODO: check to make sure it's coming from Twilio...
         var rawIn = this.request.body;
         var pstat = {};
 
@@ -53,15 +43,39 @@ if (Meteor.isServer) {
           var xml = '<Response><Sms>Found no text in status update.</Sms></Response>';
           return [500, {"Content-Type": "text/xml"}, xml];
         }
+        // deconstruct and parse the message contents
+        var parts = pstat.msg.split(" ");
+        if (parts.length < 2) {
+          var xml = '<Response><Sms>Unparsable message body:\'' + pstat.msg + '\'</Sms></Response>';
+          return [500, {"Content-Type": "text/xml"}, xml];
+        }
+        var wip = String(parts[0]);
+        var pilotID = -1;
+        if (wip.substring(0,1) == "#") {
+          pilotID = parseInt( wip.substring(1) );
+        } else {
+          pilotID = parseInt( wip );
+        }
+        var pilotMsg = parts[1];
+        var pilotNotes = "";
+        if (parts.length > 2)
+          pilotNotes = parts.slice(2).join(" ");
+
+        pstat.status = pilotMsg;
+        pstat.pilotID = parseInt(pilotID);
+        pstat.notes = pilotNotes;
         pstat.from = rawIn.From;
         pstat.date = new Date();
 
+        // add a new record to the database
         PilotStatus.insert( pstat );
 
-        console.log("jabba:"+pstat.msg);
+        // debug
+        console.log("received and parsed:" + pstat.msg);
+        console.log("pstat:" + pstat);
 
         this.response.writeHead( 200, {"Content-Type": "text/xml"} );
-        this.response.end('<Response><Sms>Acknowledged - %s</Sms></Response>' % (pstat.from));
+        this.response.end('<Response><Sms>Acknowledged - ' + pstat.from + '</Sms></Response>');
       });
   });
 }
