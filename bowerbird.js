4336fb7d752b40b216b38c061a8775e370efdd45
Gever Tulley <gever@sfbrightworks.org>
2015-09-08 23:48:38 -0700

    get live status updates working, fix css

--- bowerbird.js
+++ bowerbird.js
@@ -1,6 +1,20 @@
 PilotStatus = new Mongo.Collection('pilot_status');
 Pilots = new Mongo.Collection('pilots');
 
+StatusMap = {
+  'FLY':'status_flying',
+  'LOK':'status_landed',
+  'PUP':'status_pickedup',
+  'AHQ':'status_home',
+  'AID':'status_reqaid',
+  'RZA':'status_enroute',
+  'RZB':'status_enroute',
+  'RZC':'status_enroute',
+  'RZD':'status_enroute',
+  'NOT':'status_nottracked',
+  'UNK':'status_unknown'
+};
+
 Router.configure({
   layoutTemplate: 'main',
   notFoundTemplate: 'blank',
@@ -50,6 +64,9 @@ if (Meteor.isClient) {
   Template.pilotview.helpers({
     'pilots' : function() {
       return Pilots.find();
+    },
+    'styling' : function(st) {
+      return StatusMap[st];
     }
   });
 }
@@ -80,6 +97,34 @@ if (Meteor.isServer) {
       .get(function() {
         // clean out and reload the pilot database
         Pilots.remove({});
+        var text = Assets.getText('pilot_list.csv');
+        var plist = Baby.parse(text, {header:true});
+        console.log( "plist length: " + plist.data.length );
+        // console.log( plist );
+        var skipCount = 0;
+        for(var i=0; i<plist.data.length; i++) {
+          record = plist.data[i]
+          if (record.hasOwnProperty('Name')) {
+            record.id = parseInt(record.id, 10);
+            // default current status to NOT currently tracked
+            record.current_status = "NOT";
+            Pilots.insert( record );
+          } else {
+            skipCount++;
+          }
+        }
+        if (skipCount > 0) {
+          console.log("Skipped " + skipCount + " incomplete records.");
+        }
+        // TODO: index the pilot records by id
+        console.log( Pilots.findOne({id:7}) );
+        this.response.writeHead( 200, {"Content-Type": "text/text"} );
+        this.response.end('Okey doke.');
+      });
+    Router.route('/reload-pilots-obsolete', {where:'server'})
+      .get(function() {
+        // clean out and reload the pilot database
+        Pilots.remove({});
         var text = Assets.getText('pilots.json');
         // console.log( text );
         var plist = JSON.parse( text );
@@ -100,7 +145,7 @@ if (Meteor.isServer) {
       .get(function () {
         var msg = "";
         msg = "<html><head></head><body><pre>";
-        msg += JSON.stringify(Pilots.findOne({id:12}), true, 2);
+        msg += JSON.stringify(Pilots.findOne({id:7}), true, 2);
         msg += "</pre></body></html>";
         this.response.writeHead( 200, {"Content-Type": "text/html"} );
         this.response.end(msg);
@@ -123,6 +168,7 @@ if (Meteor.isServer) {
           var xml = '<Response><Sms>Found no text in status update.</Sms></Response>';
           return [500, {"Content-Type": "text/xml"}, xml];
         }
+
         // deconstruct and parse the message contents
         var parts = pstat.msg.split(" ");
         if (parts.length < 2) {
@@ -136,23 +182,34 @@ if (Meteor.isServer) {
         } else {
           pilotID = parseInt( wip );
         }
-        var pilotMsg = parts[1];
+        var pilotMsg = parts[1].toUpperCase();
+
+        // TODO: look for lat/lon on LOK messages
         var pilotNotes = "";
         if (parts.length > 2)
           pilotNotes = parts.slice(2).join(" ");
 
-        pstat.status = pilotMsg;
+        pstat.current_status = pilotMsg;
         pstat.pilotID = parseInt(pilotID);
         pstat.notes = pilotNotes;
         pstat.from = rawIn.From;
         pstat.date = new Date();
 
-        // add a new record to the database
+        // add a new record to the status database
         PilotStatus.insert( pstat );
 
+        // update the corresponding record to show latest status
+        Pilots.update(
+          { id: pstat.pilotID },
+          { $set: {
+            current_status: pstat.current_status,
+            date: new Date()
+            }
+          });
+
         // debug
+        // console.log("pstat:" + pstat);
         console.log("received and parsed:" + pstat.msg);
-        console.log("pstat:" + pstat);
 
         this.response.writeHead( 200, {"Content-Type": "text/xml"} );
         this.response.end('<Response><Sms>Acknowledged - ' + pstat.from + '</Sms></Response>');
