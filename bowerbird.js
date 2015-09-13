d56ec32d6150957180fe957ef105a9cfe2e1d5a2
Gever Tulley <gever@sfbrightworks.org>
2015-09-13 01:50:29 -0700

    progress on many fronts

--- bowerbird.js
+++ bowerbird.js
@@ -40,16 +40,14 @@ Router.route('/overview', function () {
     }
   });
 });
-/* Router.route('/all'); */
 
-Router.map( function () {
-  this.route('all');
-});
-Router.map( function () {
-  this.route('one');
-});
-Router.map( function () {
-  this.route('admin');
+Router.route('/pilotview/:pid', function () {
+  this.render('pilotview', {
+    data: function () {
+      var p = Pilots.findOne({id: parseInt(this.params.pid)});
+      return p;
+    }
+  });
 });
 
 /*
@@ -70,14 +68,20 @@ if (Meteor.isClient) {
       return StatusMap[st];
     }
   });
+  Template.pilotview.helpers({
+    'styling' : function(st) {
+      return StatusMap[st];
+    }
+  });
 }
 
 /*
  * SERVER SIDE
  */
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  var latlon_re = /[-]*[0-9]+\.[0-9]+/g;
 
+  Meteor.startup(function () {
     // code to run on server at startup
     PilotStatus.allow({
       insert: function (userId, post) {
@@ -107,8 +111,14 @@ if (Meteor.isServer) {
           record = plist.data[i]
           if (record.hasOwnProperty('Name')) {
             record.id = parseInt(record.id, 10);
+
             // default current status to NOT currently tracked
             record.current_status = "NOT";
+
+            // default current location to unknown
+            record.current_lat = 0;
+            record.current_lon = 0;
+
             Pilots.insert( record );
           } else {
             skipCount++;
@@ -118,7 +128,7 @@ if (Meteor.isServer) {
           console.log("Skipped " + skipCount + " incomplete records.");
         }
         // TODO: index the pilot records by id
-        console.log( Pilots.findOne({id:7}) );
+        // console.log( Pilots.findOne({id:7}) );
         this.response.writeHead( 200, {"Content-Type": "text/text"} );
         this.response.end('Okey doke.');
       });
@@ -171,7 +181,8 @@ if (Meteor.isServer) {
         }
 
         // deconstruct and parse the message contents
-        var parts = pstat.msg.split(" ");
+        // var parts = pstat.msg.split(" ");
+        var parts = pstat.msg.trim().split(" ");
         if (parts.length < 2) {
           var xml = '<Response><Sms>Unparsable message body:\'' + pstat.msg + '\'</Sms></Response>';
           return [500, {"Content-Type": "text/xml"}, xml];
@@ -185,7 +196,16 @@ if (Meteor.isServer) {
         }
         var pilotMsg = parts[1].toUpperCase();
 
-        // TODO: look for lat/lon on LOK messages
+        // look for lat/lon on LOK messages
+        var ll = pstat.msg.match(latlon_re);
+        console.log("ll = " + ll.length + ", " + JSON.stringify(ll)); 
+        var got_ll_update = false;
+        if (ll.length == 2) {
+          console.log( "Jabba!" );
+          pstat.current_lat = parseFloat(ll[0]);
+          pstat.current_lon = parseFloat(ll[1]);
+          got_ll_update = true;
+        }
         var pilotNotes = "";
         if (parts.length > 2)
           pilotNotes = parts.slice(2).join(" ");
@@ -200,13 +220,25 @@ if (Meteor.isServer) {
         PilotStatus.insert( pstat );
 
         // update the corresponding record to show latest status
-        Pilots.update(
-          { id: pstat.pilotID },
-          { $set: {
-            current_status: pstat.current_status,
-            date: new Date()
-            }
-          });
+        if (got_ll_update) {
+          Pilots.update(
+            { id: pstat.pilotID },
+            { $set: {
+              current_status: pstat.current_status,
+              date: new Date(),
+              current_lat: pstat.current_lat,
+              current_lon: pstat.current_lon
+              }
+            });
+        } else {
+          Pilots.update(
+            { id: pstat.pilotID },
+            { $set: {
+              current_status: pstat.current_status,
+              date: new Date()
+              }
+            });
+        }
 
         // debug
         // console.log("pstat:" + pstat);
