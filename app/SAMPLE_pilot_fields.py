bb98c9c30401c9ec453d307b3afa6863f095ad6d
root <root@bowerbird2021-ao.c.bowerbird2018-204919.internal>
2021-05-06 05:09:18 +0000

    updated to use generic LABEL reference in HTML

--- app/SAMPLE_pilot_fields.py
+++ app/SAMPLE_pilot_fields.py
@@ -1,37 +1,25 @@
 # pilot status record field names (trying to isolate from CSV dependencies a little bit)
 LABEL_PNUM = 'Pilot#' # space gets removed when all column header spaces are removed in parse_pilot_record
-LABEL_PID = "PilotID" # more easily parseable/codeable label for the Pilot Number
 LABEL_STATUS = 'STATUS'
 LABEL_FNAME = 'FirstName'
 LABEL_LNAME = 'LastName'
 LABEL_PHONE = 'Telephone'
 LABEL_LAT = 'Lat'
 LABEL_LON = 'Lon'
-LABEL_EVENT = 'Event'
-LABEL_COUNTRY = 'Country'
-LABEL_CITY = 'City'
-LABEL_STATE = 'State'
-LABEL_PHONE = 'Telephone'
-LABEL_EMAIL = 'Email'
-LABEL_FAI = 'FAI'
-LABEL_DOB = 'DOB'
+LABEL_URL = 'URL' # satellite tracking page 
+LABEL_TRACKER = 'Tracker' # ID for tracker such as Flymaster Live Tracker, or code for trackupload
 LABEL_GLIDER_MFG = 'GliderManufacturer'
 LABEL_GLIDER_MODEL = 'GliderModel'
 LABEL_GLIDER_COLORS = 'Colors'
 LABEL_GLIDER_RATING = 'Rating' # Glider EN rating
+LABEL_EVENT = 'Event'
+# the values below here are not explicitly listed in any HTML right now, i believe
+LABEL_COUNTRY = 'Country'
+LABEL_CITY = 'City'
+LABEL_STATE = 'State'
+LABEL_EMAIL = 'Email'
+LABEL_FAI = 'FAI'
+LABEL_DOB = 'DOB'
 LABEL_SPONSOR = 'Sponsor'
 LABEL_ISPAID = 'IsPaid'
-LABEL_URL = 'URL' # satellite tracking page 
-LABEL_TRACKER = 'Tracker' # ID for tracker such as Flymaster Live Tracker
-LABEL_DRIVER = 'Driver'
 
-#contact info labels
-LABEL_PRESETINDEX = 'PresetIndex'
-LABEL_CONTACTINFO = 'ContactInfo'
-LABEL_DEVICEMODEL = 'Model'
-
-#staff labels
-LABEL_ROLE = "Role"
-LABEL_STAFF_NAME = "Name"
-LABEL_STAFF_PHONE = "Telephone"
-LABEL_STAFF_PROVIDER = "TelephoneProvider"
